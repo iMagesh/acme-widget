23183d08c05ebfc29e1891c0ac562a5fd785b219
Magesh <pingmagesh@gmail.com>
2025-07-19 00:52:31 -0400

    refactor: Utilize retail_round method for subtotal calculation in BuyOneGetOneHalfPrice class for improved consistency

--- offers/base_offer.rb
+++ offers/base_offer.rb
@@ -3,4 +3,9 @@ class BaseOffer
   def apply(item_counts, catalogue)
     0.0 # Default: no discount
   end
+
+  # Shared retail rounding utility
+  def retail_round(amount)
+    BigDecimal(amount.to_s).truncate(2)
+  end
 end
