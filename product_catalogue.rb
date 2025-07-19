5b389b686fafa29d852f6d59ca94bea41060d20b
Magesh <pingmagesh@gmail.com>
2025-07-19 01:34:57 -0400

    refactor: Rename variable 'prod' to 'product' for improved clarity in price method

--- product_catalogue.rb
+++ product_catalogue.rb
@@ -6,8 +6,8 @@ class ProductCatalogue
 
   def price(code)
     raise UnknownProductCodeError, "Unknown product code: #{code}" unless valid_code?(code)
-    prod = @products[code.to_s]
-    prod ? prod['price'] || prod[:price] : nil
+    product = @products[code.to_s]
+    product ? product['price'] || product[:price] : nil
   end
 
   def valid_code?(code)
